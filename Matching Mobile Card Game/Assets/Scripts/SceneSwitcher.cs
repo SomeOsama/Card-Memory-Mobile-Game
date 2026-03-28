09b2573b3a3e36b11450a4a45a774d590165820c
Sara <sararacel245@gmail.com>
2026-03-27 23:28:24 -0400

    Updated UI
    - Added 3 scenes from the project I was having trouble uploading to GitHub desktop
         - Vertical and horizontal versions of each screen
         - Most things scale with screen size now
    - Added a horizontal version of the wood background .png
    - Deleted "Menu.cs" and added "SceneSwitcher.cs" to replace it
         - Used with buttons to switch scenes and close the game
    - Added a new panel with image to the Canvas in the Main scene so that the blue can't be seen

--- Matching Mobile Card Game/Assets/Scripts/SceneSwitcher.cs
+++ Matching Mobile Card Game/Assets/Scripts/SceneSwitcher.cs
@@ -1,29 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class SceneSwitcher : MonoBehaviour
 {
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
     public void Play()
     {
-        SceneManager.LoadScene("DifficultySelect");
+        SceneManager.LoadScene("Main");
     }
 
     public void Settings()
     {
         SceneManager.LoadScene("Settings");
     }
-
     public void Credits()
     {
         SceneManager.LoadScene("Credits");
     }
-    public void Back()
-    {
-        SceneManager.LoadScene("Menu");
-    }
-    public void Quit()
+    public void Exit()
     {
         Application.Quit();
     }
